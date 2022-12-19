301f333c43677234ac414a36f42a972b09365426
Michael <Mich4elcoop321@gmail.com>
2022-12-19 18:04:55 -0500

    responsive update1

--- src/components/layout/about/AboutCard.js
+++ src/components/layout/about/AboutCard.js
@@ -7,38 +7,35 @@ const AboutCard = () => {
       <div className={classes["about-cards"]}>
         <h1>History</h1>
         <p>
-          {" "}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Daydream Cookies was first founded in 1924. The Original owner Michael
+          Cooperman started in his own kitchen where he found his true passion
+          for cookies. He would go around his local town sharing his cookies to
+          get people to vote on their favorite recipe. Once he had enough people
+          test the cookies there was a clear winner. To this day the secret
+          ingredient put into the cookies has not been released to the public.
         </p>
       </div>
       <div className={classes["about-card-middle"]}>
         <h1>Our Mission</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          One of the main goals of Daydream Cookies is to have the customers
+          select various different flavors of cookies to add to their own custom
+          order list. With this order list we will deliver at random (up to 5
+          times a month) and we will ship them out as a surprise box. At a low
+          montly price depending on the package you sign up for. Here at
+          Daydream cookies we want you to have the best cookie experience ever!
         </p>
       </div>
       <div className={classes["about-cards"]}>
-        <h1>Important Cookie Information</h1>
+        <h1>Fun Cookie Information</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Daydream cookies have over 100,000 cookies baked per month. All
+          cookies are delievered fast and keep their warmth throughout the trip
+          to your house. Ever wonder how we ship the cookies so fast? Well we
+          now use drones! With new advancements in technology we have over 1,000
+          cookie drones that deliver within 4 hours of your order being placed.
+          Another fun fact is Daydream cookies is the first cookie company to go
+          international. Order any and where any time.
         </p>
       </div>
     </div>
